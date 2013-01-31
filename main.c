3caf49d44f5eca1049053fc7cc8e9f57b94857f9
Aleksey-Devyataikin <Aleksey.Devyataikin@gmail.com>
2013-01-31 12:32:14 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,5 +2,37 @@
 
 void main(void)
 {
-	printf("Hello!\n");
+	int type = 0;
+	int result = 0;
+	float input = 0, output = 0;
+	printf("Select trasformation type:\n1\tC->F\n2\tF->C\ntype: ");
+	result = scanf("%d", &type);
+	if (result != 1)
+	{
+		printf("Unable to read digit. Goodbye!");
+		return;
+	}
+	if (type > 2 || type < 1)
+	{
+		printf("Incorrect transformation type. Goodbye!");
+		return;
+	}
+	printf("Temperature in %c: ", type == 1 ? "C" : "F");
+	result = scanf("%f", &input);
+	if (result != 1)
+	{
+		printf("Incorrect temperature. Goodbye!");
+		return;
+	}
+	switch (type)
+	{
+	case 1: 
+		output = 32 + ((input * 9) / 5);
+		break;
+	case 2: 
+		output = (input - 32) * 5 / 9;
+		break;
+	}
+	printf("Temperature in %c: %f\d", type == 1 ? "F" : "C", output);
+	
 }
